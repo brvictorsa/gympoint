ff1b2ae631e3c608cccaeb183cd3b5de7d0b33b0
Victor Santos <victorsa.mg@gmail.com>
2020-01-08 01:22:17 -0300

    Removido ícone não utilizado.

--- frontend/src/pages/HelpOrder/index.js
+++ frontend/src/pages/HelpOrder/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { FaPlus, FaSyncAlt } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
 
 import api from '../../services/api';
 
